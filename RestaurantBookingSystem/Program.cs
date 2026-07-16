3291147d3f8346eb685ed5ce47f1c723325bb64a
Minh <duong22112004@gmail.com>
2026-07-16 14:51:13 +0700

    Example Structure

--- RestaurantBookingSystem/Program.cs
+++ RestaurantBookingSystem/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using RestaurantBookingSystem.Models;
+using RestaurantBookingSystem.Features.Notification.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,11 @@ builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<RestaurantReservationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<INotificationService, NotificationService>();
+
+// ...
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
