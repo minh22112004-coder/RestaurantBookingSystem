37f12eb95e930cb833a1d4dcccf015a4394eec39
Vinh Quang <quangvinh02112004@gmail.com>
2026-08-02 14:21:55 +0700

    Add menu CRUD APIs

---
+++ RestaurantBookingSystem/Controllers/MenuItemController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestaurantBookingSystem.Models;
+
+namespace RestaurantBookingSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuItemController : ControllerBase
+    {
+        private readonly RestaurantReservationDbContext _context;
+
+        public MenuItemController(RestaurantReservationDbContext context)
+        {
+            _context = context;
+        }
+
+        // get: api/MenuItem
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MenuItem>>> GetMenuItems()
+        {
+            return await _context.MenuItems
+                .Include(m => m.Category)
+                .Include(m => m.Restaurant)
+                .ToListAsync();
+        }
+
+        // get: api/MenuItem/1
+        [HttpGet("{id}")]
+        public async Task<ActionResult<MenuItem>> GetMenuItem(int id)
+        {
+            var menuItem = await _context.MenuItems
+                .Include(m => m.Category)
+                .Include(m => m.Restaurant)
+                .FirstOrDefaultAsync(m => m.MenuItemId == id);
+
+            if (menuItem == null)
+            {
+                return NotFound();
+            }
+
+            return menuItem;
+        }
+
+        // post: api/MenuItem
+        [HttpPost]
+        public async Task<ActionResult<MenuItem>> CreateMenuItem(MenuItem menuItem)
+        {
+            _context.MenuItems.Add(menuItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(
+                nameof(GetMenuItem),
+                new { id = menuItem.MenuItemId },
+                menuItem
+            );
+        }
+
+        // put: api/MenuItem/1
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateMenuItem(
+            int id,
+            MenuItem menuItem)
+        {
+            if (id != menuItem.MenuItemId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(menuItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.MenuItems.Any(e => e.MenuItemId == id))
+                {
+                    return NotFound();
+                }
+
+                throw;
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMenuItem(int id)
+        {
+            var menuItem = await _context.MenuItems.FindAsync(id);
+
+            if (menuItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.MenuItems.Remove(menuItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
